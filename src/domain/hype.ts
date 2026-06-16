66e01829a995e66a97f55c6e48bfeee06ad48b68
Mart van de Ven <m@type.hk>
2026-06-16 14:46:11 +0800

    refactor: remove dead cols

--- src/domain/hype.ts
+++ src/domain/hype.ts
@@ -1,27 +1,19 @@
 export interface HypeUser {
-  email: string;
-  id: string;
+	email: string;
+	id: string;
 }
 
 export interface HypeRow {
-  [key: string]: string;
-  status: "OK" | "ERROR" | "SKIP";
-  error: string;
-  "feature.id": string;
-  "layer.name": string;
-  "feature.latitude": string;
-  "feature.longitude": string;
-  "feature.i18n.locale": string;
-  "feature.i18n.featureId": string;
-  "feature.i18n.title": string;
-  "feature.i18n.titleGen": string;
-  "feature.i18n.description": string;
-  "feature.i18n.descriptionGen": string;
-  "feature.i18n.rawAddress": string;
-  "feature.isArchived": string;
-  "feature.isIntangible": string;
-  "feature.isPublished": string;
-  "feature.isVisitable": string;
-  "user.email": string;
-  "user.id": string;
+	[key: string]: string;
+	status: "OK" | "ERROR" | "SKIP";
+	error: string;
+	"feature.id": string;
+	"feature.latitude": string;
+	"feature.longitude": string;
+	"feature.isArchived": string;
+	"feature.isIntangible": string;
+	"feature.isPublished": string;
+	"feature.isVisitable": string;
+	"user.email": string;
+	"user.id": string;
 }
